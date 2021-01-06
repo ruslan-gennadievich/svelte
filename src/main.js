0fff98c62225820200821c7eb5ca3b446b596335
ruslan-gennadievich <rusya@mail.ru>
2021-01-06 04:54:48 +0200

    Update main.js
    Test Tele

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import App from './App.svelte';
+import { Context, Telegraf } from 'telegraf'
+import session from 'telegraf/session'
 
 const app = new App({
 	target: document.body,
@@ -7,36 +9,15 @@ const app = new App({
 	}
 });
 
-import { Context, Telegraf } from 'telegraf'
-import session from 'telegraf/session'
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome2'))
+bot.help((ctx) => ctx.reply('Send me a sticker!'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Aee Hey there'))
+bot.launch()
 
-interface SessionData {
-  lastMessageId?: number
-  photoCount?: number
-  // ... more session data go here
-}
-
-// Define your own context type
-interface MyContext extends Context {
-  session?: SessionData
-  // ... more props go here
-}
-
-// Create your bot and tell it about your context type
-const bot = new Telegraf<MyContext>(process.env.BOT_TOKEN)
-
-// Make session data available
-bot.use(session())
-// Register middleware and launch your bot as usual
-bot.use((ctx, next) => {
-  // Yay, `session` is now available here as `SessionData`!
-  if (ctx.message !== undefined)
-    ctx.session.lastMessageId = ctx.message.message_id
-  return next()
-})
-bot.on('photo', (ctx, next) => {
-  ctx.session.photoCount = 1 + (ctx.session.photoCount ?? 0)
-  return next()
-})
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 export default app;
