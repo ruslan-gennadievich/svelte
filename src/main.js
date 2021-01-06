fdedf2393fca90ac3c109fe8fd8c10325c14defa
ruslan-gennadievich <rusya@mail.ru>
2021-01-06 05:02:40 +0200

    Update main.js
    test2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import App from './App.svelte';
-import { Context, Telegraf } from 'telegraf'
+
 import session from 'telegraf/session'
 
 const app = new App({
@@ -9,15 +9,5 @@ const app = new App({
 	}
 });
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome2'))
-bot.help((ctx) => ctx.reply('Send me a sticker!'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Aee Hey there'))
-bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 export default app;
